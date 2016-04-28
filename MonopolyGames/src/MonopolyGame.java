d6962cf3943ea18e4b0bb9452cf05f4ccc32e4e5
cluselm <cluselm@PC-B123-03.iut-valence.fr>
2016-04-28 13:01:48 +0200

    DEUXIEME

--- MonopolyGames/src/MonopolyGame.java
+++ MonopolyGames/src/MonopolyGame.java
@@ -1,13 +1,13 @@
 
 public class MonopolyGame
 {
-	private final int x;  
-	private final int y;
+	private final int pion;  
+	private final int lol;
 	
 	public MonopolyGame(int x0, int y0)
 	{
-		this.y = y0;
-		this.y = y0;
+		this.pion = y0;
+		this.y=lol = y0;
 		
 	}
 
