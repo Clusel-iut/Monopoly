78a9b6bbff12cf0a4d8d1e7fc87d277c31f264d1
cluselm <cluselm@PC-B123-03.iut-valence.fr>
2016-04-28 14:40:52 +0200

    **

---
+++ MonopolyGames/src/fr/iutvalence/info/dut/m2107/AllerEnPrison.java
@@ -0,0 +1,17 @@
+package fr.iutvalence.info.dut.m2107;
+
+/**
+ * @author cluselm
+ *
+ */
+public class AllerEnPrison extends Case {
+
+	/**
+	 * 
+	 */
+	public void allerEnPrison() {
+		// TODO - implement AllerEnPrison.allerEnPrison
+		throw new UnsupportedOperationException();
+	}
+
+}
