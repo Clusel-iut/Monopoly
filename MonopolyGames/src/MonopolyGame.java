bcf3d68fa9b16f04ae6b6eb0a37c066b53b3c19b
cluselm <cluselm@PC-B123-03.iut-valence.fr>
2016-04-28 12:55:18 +0200

    PREMIER

---
+++ MonopolyGames/src/MonopolyGame.java
@@ -0,0 +1,14 @@
+
+public class MonopolyGame
+{
+	private final int x;  
+	private final int y;
+	
+	public MonopolyGame(int x0, int y0)
+	{
+		this.y = y0;
+		this.y = y0;
+		
+	}
+
+}
---
+++ MonopolyGames/src/Rack.java
@@ -0,0 +1,11 @@
+
+public class Rack
+{
+	
+	public Rack()
+	{
+		return false;
+		
+	}
+
+}
