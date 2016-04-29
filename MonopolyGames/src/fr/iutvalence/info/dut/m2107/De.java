8c7b498f4e71eeb81ecdfdcee92a243d239b2c72
cluselm <cluselm@PC-A102-09.iut-valence.fr>
2016-04-29 15:05:51 +0200

    Add java doc on classes #2

--- MonopolyGames/src/fr/iutvalence/info/dut/m2107/De.java
+++ MonopolyGames/src/fr/iutvalence/info/dut/m2107/De.java
@@ -1,6 +1,7 @@
 package fr.iutvalence.info.dut.m2107;
 
 /**
+ * Represents the dice of the game
  * @author cluselm
  *
  */
@@ -9,15 +10,25 @@ public class De {
 	private int valeur_de1;
 	private int valeur_de2;
 
-	public void lancerDe() {
-		// TODO - implement D�.lancerD�
+	
+	
+	/**
+	 * launch 2 dices and 
+	 * @return an integer between 1 and 6
+	 * The player advance with int cases. 
+	 */
+	@SuppressWarnings("javadoc")
+	public int lancerDe() {
+		
 		throw new UnsupportedOperationException();
 	}
 
 	/**
+	 * if a player does a 2 sames intergers, he relaunches dices  
+	 * @return true 
 	 * 
 	 */
-	public void IsDouble() {
+	public boolean IsDouble() {
 		// TODO - implement D�.IsDouble
 		throw new UnsupportedOperationException();
 	}
