da3a1d5b79fd7914cf61dcbdafead27d19d6038a
cluselm <cluselm@PC-A105-01.iut-valence.fr>
2016-05-19 08:57:24 +0200

    19/05/2016
    Update on the class Money

--- MonopolyGames/src/fr/iutvalence/info/dut/m2107/Argent.java
+++ MonopolyGames/src/fr/iutvalence/info/dut/m2107/Argent.java
@@ -7,7 +7,58 @@ package fr.iutvalence.info.dut.m2107;
  * @author martithi
  *
  */
+
 public class Argent
 {
+	
+	/**
+	 * Represent the money
+	 * @author cluselm
+	 */
+	private int money;
+	
+	
+	/**
+	 * Represents the money of all players at the start
+	 * @author cluselm
+	 */
+	private static final int DEFAULT_MONEY=1500;
+	
+	/**
+	 * represents the money of the player 1
+	 * @author cluselm
+	 */
+	private int money_player1;
+	
+	/**
+	 * represents the money of the player 2
+	 * @author cluselm
+	 */
+	private int money_player2;	
+	
 
+	
+	
+	/**
+	 * @return the money of the player.
+	 * @author cluselm
+	 */
+	public int getMoney()
+	
+	{
+		return this.money;
+	}
+	
+	/**
+	 * @param money
+	 * @author cluselm
+	 */
+	public void addORdelete(int money)
+	
+	{
+		this.money = (this.money + money);
+		
+	}
+	
+	
 }
